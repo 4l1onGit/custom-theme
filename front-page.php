835fd90dcad5f705398145dbc52382b6128ebbd1
Hassan <hali@hasalidev.online>
2026-01-11 14:12:37 +0000

    Add latest blog posts section to front page and update styles

--- front-page.php
+++ front-page.php
@@ -16,16 +16,48 @@ get_header();
         </button>
 
     </div>
-    <div class="md:col-span-6 col-span-12 h-[40vh] bg-yellow-200">
-        <div class="container mx-auto h-full flex flex-col justify-center items-center">
+    <div class="md:col-span-6 col-span-12 h-[40vh] bg-gray-200">
+        <div class="container mx-auto h-full flex flex-col justify-center px-8">
 
-            <h2 class="text-4xl font-bold mb-6">
-                Placeholder
+            <h2 class="text-2xl font-semibold mb-6 text-center">
+                Latest Blog Posts
             </h2>
-            <p class="text-lg text-center max-w-xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-            </p>
+            <?php
+            // Query for latest blog posts
+            $latest_posts = new WP_Query(
+                array(
+                    'posts_per_page' => 2,
+                  
+                )
+            );
+            while( $latest_posts->have_posts() ) {
+                $latest_posts->the_post();
+                ?>
+
+            <div class=" mb-4 w-full bg-slate-300 p-4 rounded-md space-y-2">
+                <h3 class="text-lg"><a class="text-purple-500 underline"
+                        href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <p class="text-sm max-w-xl">
+                    <?php echo wp_trim_words( get_the_content(), 20 ); ?> <span
+                        class="text-xs text-purple-600 underline font-light"><a href="<?php the_permalink(); ?>">Read
+                            More</a></span>
+                </p>
+                <span class="text-xs font-light"><?php the_date(); ?></span>
+            </div>
+
+            <?php
+            }
+            wp_reset_postdata()
+            
+            ?>
+            <button
+                class="bg-purple-600 text-md text-white px-4 py-2 rounded-lg hover:bg-purple-700 transition-all duration-105 mt-4 self-center">
+                <a href="<?php echo site_url('/blog'); ?>">
+
+                    View All Posts
+                </a>
+            </button>
+
         </div>
     </div>
     <div class="md:col-span-6 col-span-12 h-[40vh] bg-orange-200">
